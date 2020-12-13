a81151149e5fbcd7b0f9a1d3d0d82db292cd4713
James Johnston <james@ezmuze.co.uk>
2020-12-13 18:50:33 +0000

    remove serial stuff

--- MadLed/MadLed.ino
+++ MadLed/MadLed.ino
@@ -2,7 +2,7 @@
 
 class LEDDevice {
   public:
-    int numberOfLeds;
+    uint8_t numberOfLeds;
     byte pin;
     String name;
     CRGB* leds;
@@ -20,6 +20,8 @@ class LEDDevice {
     }
 };
 
+#include "HID-Project.h"
+
 #define DP1 1
 #define DP2 2
 #define DP3 3
@@ -29,273 +31,165 @@ class LEDDevice {
 #define DP7 7
 #define DP8 8
 
-char rx_byte = 0;
-String rx_str = "";
-String tx_str = "";
-bool showRequired = false;
 LEDDevice ledDevices[8];
 
+const int pinLed = LED_BUILTIN;
+const int pinButton = 2;
+
+uint8_t rawhidData[64];
+
 void setup() {
-  Serial.begin(19200);
-  Serial.println("MadLed by Mad Ninja / James Johnston 2020");
-  Serial.println("-----------------------------------------");
-  Serial.println("https://github.com/madninjaskillz/MadLed");
+
+  //Serial.begin(115200);
+
+  // Set the RawHID OUT report array.
+  // Feature reports are also (parallel) possible, see the other example for this.
+  delay(500);
+
+  RawHID.begin(rawhidData, sizeof(rawhidData));
 }
 
-String getValue(String data, char separator, int index)
-{
-  int found = 0;
-  int strIndex[] = { 0, -1 };
-  int maxIndex = data.length() - 1;
+uint8_t reportData[64];
 
-  for (int i = 0; i <= maxIndex && found <= index; i++) {
-    if (data.charAt(i) == separator || i == maxIndex) {
-      found++;
-      strIndex[0] = strIndex[1] + 1;
-      strIndex[1] = (i == maxIndex) ? i + 1 : i;
-    }
+void setupDevice(byte intDataPin, uint8_t intNoOfLed) {
+  ledDevices[intDataPin - 1].numberOfLeds = intNoOfLed;
+  ledDevices[intDataPin - 1].leds = new CRGB[intNoOfLed];
+  switch (intDataPin)
+  {
+
+    case 1:
+            FastLED.addLeds<WS2812B, DP1>(ledDevices[intDataPin - 1].leds, intNoOfLed);
+      break;
+
+    case 2:
+            FastLED.addLeds<WS2812B, DP2>(ledDevices[intDataPin - 1].leds, intNoOfLed);
+      break;
+
+    case 3:
+            FastLED.addLeds<WS2812B, DP3>(ledDevices[intDataPin - 1].leds, intNoOfLed);
+      break;
+
+    case 4:
+            FastLED.addLeds<WS2812B, DP4>(ledDevices[intDataPin - 1].leds, intNoOfLed);
+      break;
+
+    case 5:
+            FastLED.addLeds<WS2812B, DP5>(ledDevices[intDataPin - 1].leds, intNoOfLed);
+      break;
+
+    case 6:
+            FastLED.addLeds<WS2812B, DP6>(ledDevices[intDataPin - 1].leds, intNoOfLed);
+      break;
+
+    case 7:
+            FastLED.addLeds<WS2812B, DP7>(ledDevices[intDataPin - 1].leds, intNoOfLed);
+      break;
+
+    case 8:
+            FastLED.addLeds<WS2812B, DP8>(ledDevices[intDataPin - 1].leds, intNoOfLed);
+      break;
   }
-  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+
+  for (int f = 0; f < intNoOfLed; f++)
+  {
+    ledDevices[intDataPin - 1].setLED(f, 255, 0, 0);
+  }
+  FastLED.show();
+  delay(500);
+
+  for (int f = 0; f < intNoOfLed; f++)
+  {
+    ledDevices[intDataPin - 1].setLED(f, 0, 255, 0);
+  }
+  FastLED.show();
+  delay(500);
+
+  for (int f = 0; f < intNoOfLed; f++)
+  {
+    ledDevices[intDataPin - 1].setLED(f, 0, 0, 255);
+  }
+  FastLED.show();
+  delay(500);
+
+  for (int f = 0; f < intNoOfLed; f++)
+  {
+    ledDevices[intDataPin - 1].setLED(f, 0, 0, 0);
+  }
+  FastLED.show();
+  delay(500);
 }
 
-void sprintln(String input)
-{
-  tx_str = tx_str + input + "\r\n";
-}
-
-void sprint(String input)
-{
-  tx_str = tx_str + input;
-}
-
-void processCommand(String input)
-{
-  //sprintln(">"+input);
-  String cmd = getValue(rx_str, ':', 0);
-  if (cmd == "")
+void loop() {
+  // Check if there is new data from the RawHID device
+  uint8_t bytesAvailable = RawHID.available();
+  if (bytesAvailable)
   {
-    cmd = rx_str;
-  }
-
-  if (cmd == "ping")
-  {
-    sprintln("pong");
-  }
-
-  if (cmd == "ning")
-  {
-    sprintln("nong");
-  }
-
-  /*
-    if (cmd == "help") {
-
-      Serial.println("MadLed by Mad Ninja / James Johnston 2020");
-      Serial.println("-----------------------------------------");
-      Serial.println("https://github.com/madninjaskillz/MadLed");
-      Serial.println("add:{bank}:{datapin}:{ledCount}:[name]");
-      Serial.println("add a fan in bank [bank] on datapin [datapin] with [ledCount] leds called '[name]'");
-      Serial.println("  bank: bank/device number from 0 to 7");
-      Serial.println("  datapin: which arduino pin is the fan data line connected to (1 to 8)");
-      Serial.println("  ledCount: how many LEDs does the fan have?");
-      Serial.println("  name: Friendly name");
-      Serial.println("eg: add:0:3:21:Top Front");
-      Serial.println("");
-      Serial.println("set:[bank]:[led]:[Red]:[Green]:[Blue]");
-      Serial.println("set led [led] on device [bank] to R/G/B");
-      Serial.println("  bank: bank/device number from 0 to 7");
-      Serial.println("  led: led number");
-      Serial.println("  Red/Green/Blue: value from 0 to 255");
-      Serial.println("eg: set:0:15:255:0:255");
-      Serial.println("  ");
-      Serial.println("get:[bank]:[led]");
-      Serial.println("Get the rgb in format RRR:GGG:BBB of led [led] on device [bank]");
-      Serial.println("  bank: bank/device number from 0 to 7");
-      Serial.println("  led: led number");
-      Serial.println("eg: get:0:15");
-    }*/
-
-  if (cmd == "set")
-  {
-
-    String cmdBank = getValue(input, ':', 1);
-    String cmdLed = getValue(input, ':', 2);
-    String cmdR = getValue(input, ':', 3);
-    String cmdG = getValue(input, ':', 4);
-    String cmdB = getValue(input, ':', 5);
-
-    int intBank = cmdBank.toInt();
-    int intLed = cmdLed.toInt();
-    int intR = cmdR.toInt();
-    int intG = cmdG.toInt();
-    int intB = cmdB.toInt();
-
-    ledDevices[intBank].setLED(intLed, intR, intG, intB);
-    showRequired = true;
-  }
-
-  if (cmd == "bulk")
-  {
-
-    String cmdBank = getValue(input, ':', 1);
-    String cmdLed = getValue(input, ':', 2);
-    String cmdR = getValue(input, ':', 3);
-    String cmdG = getValue(input, ':', 4);
-    String cmdB = getValue(input, ':', 5);
-
-    String cmdLed1 = getValue(cmdLed, '-', 0);
-    String cmdLed2 = getValue(cmdLed, '-', 1);
-
-    int intBank = cmdBank.toInt();
-    int intLed1 = cmdLed1.toInt();
-    int intLed2 = cmdLed2.toInt();
-    int intR = cmdR.toInt();
-    int intG = cmdG.toInt();
-    int intB = cmdB.toInt();
-
-    for (int i = intLed1; i <= intLed2; i++)
+    int ct = 0;
+    bool anydata = false;
+    while (bytesAvailable--)
     {
-      ledDevices[intBank].setLED(i, intR, intG, intB);
+      reportData[ct] = RawHID.read();
+      if (reportData[ct] > 0 && ct > 0) {
+        anydata = true;
+      }
+      ct++;
     }
 
-    showRequired = true;
-  }
+    if (anydata) {
+      char cmd = reportData[0];
 
-  if (cmd == "get")
-  {
+      if (cmd == 0)
+      {
+        byte intDataPin = reportData[1];
+        uint8_t intNoOfLed = reportData[2];
+        uint8_t deviceclass = reportData[3];
 
-    String cmdBank = getValue(input, ':', 1);
-    String cmdLed = getValue(input, ':', 2);
+        char name[16];
+        for (int i = 0; i < 16; i++) {
+          name[i] = reportData[4 + i];
+        }
 
-    int intBank = cmdBank.toInt();
-    int intLed = cmdLed.toInt();
+        setupDevice(intDataPin, intNoOfLed);
 
-    CRGB cl = ledDevices[intBank].getLED(intLed);
+        reportData[0] = 128;
 
-    sprint(cl.r + "");
-    sprint(":");
-    sprint(cl.g + "");
-    sprint(":");
-    sprintln(cl.b + "");
-  }
+        RawHID.write(reportData, sizeof(reportData));
+      }
 
-  if (cmd == "add")
-  {
-    String cmdBank = getValue(input, ':', 1);
-    int intBank = cmdBank.toInt();
+      if (cmd == 1)
+      {
+        byte intDataPin = reportData[1];
+        byte page = reportData[2];
+        int ct = 20 * page;
+        int cp = 3;
+        for (int i = 0; i < 20; i++) {
+          byte r = reportData[cp + 0];
+          byte g = reportData[cp + 1];
+          byte b = reportData[cp + 2];
 
-    String cmdDataPin = getValue(input, ':', 2);
-    int intDataPin = cmdDataPin.toInt();
+          if (ct < ledDevices[intDataPin - 1].numberOfLeds) {
 
-    String cmdNoOfLed = getValue(input, ':', 3);
-    int intNoOfLed = cmdNoOfLed.toInt();
+            ledDevices[intDataPin - 1].setLED(ct, r, g, b);
 
-    String cmdName = getValue(input, ':', 4);
+          }
 
-    ledDevices[intBank].name = cmdName;
-    ledDevices[intBank].numberOfLeds = intNoOfLed;
-    ledDevices[intBank].leds = new CRGB[intNoOfLed];
-    switch (intDataPin)
-    {
+          ct++;
+          cp = cp + 3;
 
-      case 1:
-        FastLED.addLeds<NEOPIXEL, DP1>(ledDevices[intBank].leds, intNoOfLed);
-        break;
+        }
 
-      case 2:
-        FastLED.addLeds<NEOPIXEL, DP2>(ledDevices[intBank].leds, intNoOfLed);
-        break;
+      }
 
-      case 3:
-        FastLED.addLeds<NEOPIXEL, DP3>(ledDevices[intBank].leds, intNoOfLed);
-        break;
+      if (cmd == 2)
+      {
+  //todo
+      }
 
-      case 4:
-        FastLED.addLeds<NEOPIXEL, DP4>(ledDevices[intBank].leds, intNoOfLed);
-        break;
+      if (cmd == 3)
+      {
+        FastLED.show();
+      }
 
-      case 5:
-        FastLED.addLeds<NEOPIXEL, DP5>(ledDevices[intBank].leds, intNoOfLed);
-        break;
-
-      case 6:
-        FastLED.addLeds<NEOPIXEL, DP6>(ledDevices[intBank].leds, intNoOfLed);
-        break;
-
-      case 7:
-        FastLED.addLeds<NEOPIXEL, DP7>(ledDevices[intBank].leds, intNoOfLed);
-        break;
-
-      case 8:
-        FastLED.addLeds<NEOPIXEL, DP8>(ledDevices[intBank].leds, intNoOfLed);
-        break;
-    }
-
-    for (int f = 0; f < intNoOfLed; f++)
-    {
-      ledDevices[intBank].setLED(f, 255, 0, 0);
-    }
-    FastLED.show();
-    delay(500);
-
-    for (int f = 0; f < intNoOfLed; f++)
-    {
-      ledDevices[intBank].setLED(f, 0, 255, 0);
-    }
-    FastLED.show();
-    delay(500);
-
-    for (int f = 0; f < intNoOfLed; f++)
-    {
-      ledDevices[intBank].setLED(f, 0, 0, 255);
-    }
-    FastLED.show();
-    delay(500);
-
-    for (int f = 0; f < intNoOfLed; f++)
-    {
-      ledDevices[intBank].setLED(f, 0, 0, 0);
-    }
-    FastLED.show();
-    delay(500);
-  }
-
-  if (cmd == "devices")
-  {
-    for (int i = 0; i < 8; i++) {
-      sprint(i + "");
-      sprint(":");
-      sprint(ledDevices[i].numberOfLeds + "");
-      sprintln(":" + ledDevices[i].name);
     }
   }
-}
-int arrivingdatabyte = 0;
-void loop()
-{
-  if (Serial.available() > 0)
-  {
-    arrivingdatabyte = Serial.read( );
-
-    if (arrivingdatabyte == '\n' || arrivingdatabyte == '~') {
-      rx_str.trim();
-      processCommand(rx_str);
-      rx_str = "";
-    } else {
-      rx_str += (char)arrivingdatabyte;
-    }
-  }
-
-  if (tx_str.length() > 0) {
-    Serial.print(tx_str);
-    tx_str = "";
-
-  }
-
-
-  if (showRequired) {
-    FastLED.show();
-    showRequired = false;
-  }
-
 }
